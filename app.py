67feb5c382f369d9f91eb560439ece430a38129b
tour-mate <mineth.tourmate@gmail.com>
2023-04-29 00:47:23 +0530

    Files

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, request, jsonify
+from recomendation import *
+
+app = Flask(__name__)
+
+#http://127.0.0.1:5000/recc?location=spain&type=leisure
+# https://tour-mate.herokuapp.com/recc?location=weligama&type=leisure
+# https://github.com/zinedkaloc/ai-travel-planner
+@app.route("/recc")
+def recc():
+    location = request.args.get('location')
+    type =request.args.get('type')
+    return recommend_hotel(location,type).to_json()
